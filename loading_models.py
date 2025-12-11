87f417e2b007a6130825b7a5c5a3577eef69f023
VishalLokam <vishalvikaslokam@gmail.com>
2025-12-11 23:10:05 +0530

    instantiate fastapi application for calling local llm

--- loading_models.py
+++ loading_models.py
@@ -1,12 +1,18 @@
 from transformers import AutoModelForCausalLM, AutoTokenizer
 
 model = AutoModelForCausalLM.from_pretrained(
-    pretrained_model_name_or_path="WeiboAI/VibeThinker-1.5B"
+    pretrained_model_name_or_path="WeiboAI/VibeThinker-1.5B",
+    dtype="auto",
+    device_map="auto",
 )
 tokenizer = AutoTokenizer.from_pretrained(
     pretrained_model_name_or_path="WeiboAI/VibeThinker-1.5B"
 )
 
-input = tokenizer("Who are you?", return_tensors="pt")
+model_input = tokenizer("Who are you?", return_tensors="pt").to(model.device)
 
-print(input)
+generate_ids = model.generate(**model_input)
+
+model_output = tokenizer.batch_decode(generate_ids, skip_special_token=True)[0]
+
+print(model_output)
